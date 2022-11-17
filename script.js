8a4f211b0f39835023341db732007ba81743327f
iolabiyi <79932461+iolabiyi@users.noreply.github.com>
2022-11-17 13:14:11 -0500

    completed functionality for calculating guesses

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-let humanScore = 0;
-let computerScore = 0;
-let currentRoundNumber = 1;
+let humanScore = 0
+let computerScore = 0
+let currentRoundNumber = 1
 
 // Write your code below:
+const generateTarget = function () {
+  return Math.floor(Math.random() * 10)
+}
+
+const compareGuesses = function (humanGuess, computerGuess, targetGuess) {
+  let humanGuessDiff = Math.abs(targetGuess - humanGuess);
+  let computerGuessDiff = Math.abs(targetGuess - computerGuess);
+  return humanGuessDiff <= computerGuessDiff;
+}
+
+const updateScore = function (winner) {
+  if (winner === 'computer') {
+    computerScore++;
+  } else if (winner === 'human') {
+    humanScore++;
+  }
+}
+
+const advanceRound = () => currentRoundNumber++;
